e9de2637ca10df4ee6d57b37a9edcc8c2c068f57
Marie Seland Pettersen <mspet@equinor.com>
2025-06-30 12:14:34 +0000

    test2

--- src/tests/test_calculations.py
+++ src/tests/test_calculations.py
@@ -2,3 +2,8 @@ def test_length_of_string() -> None:
     test_string = "python"
     assert len(test_string) == 6
 
+from geo_calculator.calculations import find_average
+
+def test_find_average():
+    test_case = [1, 2, 3, 4, 5, 6]
+    assert find_average(test_case) == 3.5  
